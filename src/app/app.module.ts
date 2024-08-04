40d694e1cca7c9b4fa6ba3796ce106f3f7786745
ekumamait <ericebuluochol@gmail.com>
2024-08-04 20:50:35 +0300

    add stock screener to display table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StockScreenerComponent } from './stock-screener/stock-screener.component';
+import { FilterDialogComponent } from './filter-dialog/filter-dialog.component';
+import { BinanceService } from './binance.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockScreenerComponent,
+    FilterDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BinanceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
